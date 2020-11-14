2bd4dc9a1324f2af7a4534d0924c9c7250edf0d1
revell29 <diraapsya0@gmail.com>
2020-11-15 01:21:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
+import soal from "./soal";
+import "./App.css";
 
-function App() {
+const loading = [
+  "*ontol",
+  "k*ntol",
+  "ko*tol",
+  "kon*ol",
+  "kont*l",
+  "konto*",
+  "**ntol",
+  "*o*tol",
+  "*on*ol",
+  "*ont*l",
+  "*onto*",
+  "k**tol",
+  "k*n*ol",
+  "k*nt*l",
+  "k*nto*",
+  "ko**ol",
+  "ko*t*l",
+  "ko*to*",
+  "kon**l",
+  "kon*o*",
+  "kont**",
+];
+
+const App = (props) => {
+  const [answer, setAnswer] = useState("");
+  const [nomorSoal, setNomorSoal] = useState(0);
+  const [correct, setCorrect] = useState(false);
+  const showSoal = soal[nomorSoal];
+  useEffect(() => {
+    if (answer === showSoal.answer) {
+      setCorrect(true);
+      setAnswer("");
+      setTimeout(() => {
+        setNomorSoal((prevState) => prevState + 1);
+        setCorrect(false);
+      }, 3000);
+    }
+  }, [answer]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`wrapper ${nomorSoal >= soal.length ? "ending" : ""}`}>
+      {nomorSoal >= soal.length ? (
+        <div id="ending">
+          <h1>YOU DID IT! GOODJOB.</h1>
+          <p>
+            made with 😍 by <a href="https://github.com/dimasriat">dimasriat</a>
+          </p>
+        </div>
+      ) : (
+        <>
+          <h1 className={correct ? "correct" : ""}>
+            {correct ? `${showSoal.answer}.` : `${showSoal.pattern}?`}
+          </h1>
+          <p>{showSoal.question}</p>
+          <div className={`hideman ${correct ? "hide" : ""}`}>
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value.toLowerCase())}
+              placeholder="tulis jawaban di sini..."
+            />
+          </div>
+          <nav>
+            made with 😍 by <a href="https://github.com/revell29">apsyadira</a>
+          </nav>
+        </>
+      )}
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/soal.js
@@ -0,0 +1,140 @@
+const soal = [
+  { pattern: "kon😍ol", answer: "konyol", question: "bertingkah bodoh" },
+  {
+    pattern: "kont😍😍",
+    answer: "kontak",
+    question: "laki-laki dan perempuan berhubungan",
+  },
+  {
+    pattern: "k😍nt😍l",
+    answer: "kental",
+    question: "berhubungan dengan susu",
+  },
+  { pattern: "😍😍ntol", answer: "bentol", question: "digigit nyamuk" },
+  { pattern: "😍😍nt😍l", answer: "centil", question: "cewek-cewek genit" },
+  {
+    pattern: "😍onto😍",
+    answer: "rontok",
+    question: "berhubungan dengan rambut",
+  },
+  {
+    pattern: "😍😍ntol",
+    answer: "mantol",
+    question: "dibutuhkan kalo lagi basah",
+  },
+  {
+    pattern: "k😍nt😍😍",
+    answer: "kentut",
+    question: "tolong lihat sikon jika ingin mengeluarkan ini",
+  },
+  {
+    pattern: "😍😍ntol",
+    answer: "pentol",
+    question: "kenyal-kenyal gitu enak..",
+  },
+  { pattern: "😍😍nt😍l", answer: "bantal", question: "teman bobok" },
+  { pattern: "😍😍ntol", answer: "cantol", question: "awas nyangkut" },
+  {
+    pattern: "😍onto😍",
+    answer: "contoh",
+    question:
+      "ada yang bagus ada yang jelek. pilih yang bagus jangan yang jelek.",
+  },
+  {
+    pattern: "k😍nto😍",
+    answer: "kantor",
+    question: "berhubungan dengan karyawan",
+  },
+  { pattern: "😍😍ntol", answer: "mentol", question: "rokok dingin" },
+  {
+    pattern: "ko😍😍ol",
+    answer: "kompol",
+    question: "berhubungan dengan polisi",
+  },
+  {
+    pattern: "kon😍ol",
+    answer: "konsol",
+    question: "bisa dipake pas main game",
+  },
+  {
+    pattern: "kont😍😍",
+    answer: "kontan",
+    question: "bayar langsung cepetan",
+  },
+  {
+    pattern: "kon😍😍l",
+    answer: "konsul",
+    question: "tanya pak dokter",
+  },
+  {
+    pattern: "kont😍😍",
+    answer: "kontra",
+    question: "aku ga setuju pokoknya",
+  },
+  {
+    pattern: "kont😍😍",
+    answer: "kontes",
+    question: "berhubungan dengan lomba (misal panjang-panjangan)",
+  },
+  { pattern: "kont😍😍", answer: "kontur", question: "garis / bentuk" },
+  { pattern: "😍ont😍😍", answer: "sontak", question: "kaget" },
+  { pattern: "k😍nt😍😍", answer: "kantuk", question: "lemes, pengen bobok" },
+  { pattern: "😍😍nt😍l", answer: "bantul", question: "nama kabupaten" },
+  { pattern: "😍😍nt😍l", answer: "buntal", question: "menggembung" },
+  {
+    pattern: "😍😍nt😍l",
+    answer: "pentul",
+    question: "panjang, ujungnya bulet",
+  },
+  {
+    pattern: "😍😍nt😍l",
+    answer: "mental",
+    question: "kalau ini sakit, bisa jadi gila",
+  },
+  {
+    pattern: "😍😍nt😍l",
+    answer: "sentil",
+    question: "sakit pokoknya kalo terlalu kencang",
+  },
+  { pattern: "😍😍nt😍l", answer: "sentul", question: "buat balapan" },
+  { pattern: "😍😍nto😍", answer: "mentok", question: "gak bisa lebih" },
+  { pattern: "😍😍😍tol", answer: "pistol", question: "benda berbahaya" },
+  {
+    pattern: "kon😍😍😍",
+    answer: "konser",
+    question: "kegiatan yang dilakukan para penyanyi",
+  },
+  {
+    pattern: "k😍😍😍ol",
+    answer: "katrol",
+    question: "benda yang gampang ditarik-tarik",
+  },
+  {
+    pattern: "😍on😍😍l",
+    answer: "ponsel",
+    question: "terserah mau besar atau kecil yang penting pas di genggaman",
+  },
+  { pattern: "ko😍😍o😍", answer: "kompor", question: "buat masak" },
+  {
+    pattern: "😍on😍o😍",
+    answer: "bonyok",
+    question: "kalo berurusan dengan preman",
+  },
+  {
+    pattern: "😍on😍o😍",
+    answer: "pondok",
+    question: "orang alim diam-diam pikirannya selalu ke sini",
+  },
+  {
+    pattern: "😍😍nt😍l",
+    answer: "mantul",
+    question: "naik turun naik turun",
+  },
+  {
+    pattern: "😍😍ntol",
+    answer: "jlntol",
+    question: "biar cepet lewat sini",
+  },
+];
+
+export default soal;
